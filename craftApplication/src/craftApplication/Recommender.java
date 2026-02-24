fca769a29794996a01c74ab6e8f7c93a2cc3bc64
user <user@localhost>
2026-02-24 14:24:15 -0500

    Fixed: BUG-001 (root cause for invalid craft number)
    Fix: recommendations were printed but NOT stored in recs in the non-E branch (1,3 case)

--- craftApplication/src/craftApplication/Recommender.java
+++ craftApplication/src/craftApplication/Recommender.java
@@ -4,45 +4,69 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Recommender {
+	
 	ArrayList<Craft> recs = new ArrayList<Craft>();
+	
 	public Recommender(String input, ArrayList<String> items, Screen[] screens) {
+		
+		//case: use entire inv
 		if (input.equals("E")) {
+			
 			for (Craft craft: ((CatalogScreen) screens[2]).getItems()) {
+				
 				String[] materials = craft.getMaterials();
 				int count = 0;
+				
 				for (String mat: materials) {
 					if (!items.contains(mat)){
 						count++;
 					}
 				}
+				
+				//exact match
 				if (count==0) {
 					System.out.println(craft);
 					recs.add(craft);
 				}
-				else if (count==1 || count==2) {
+				
+				//+1 or +2 materials
+				else if (count ==1 || count==2) {
 					craft.specialPrint(items);
 					recs.add(craft);
 				}
+				
 				else {
 					System.out.println("Sorry, we could not find any crafts that match your criteria.");
 				}
 			}
 		}
+		//user selects subset
+		
 		else {
+			
 			String[] itemsUsed = input.split(",");
+			
 			for (Craft craft: ((CatalogScreen) screens[2]).getItems()) {
+				
 				String[] materials = craft.getMaterials();
 				int count = 0;
+				
 				for (String mat: materials) {
 					if (!Arrays.asList(itemsUsed).contains(mat)){
 						count++;
 					}
 				}
+				
+				//exact match
 				if (count==0) {
 					System.out.println(craft);
+					recs.add(craft); //fix: storing craft
 				}
-				else if (count==1 || count==2) {
+				
+				//+1 or 2 materials 
+				else if (count == 1 || count == 2) {
 					craft.specialPrint(items);
+					recs.add(craft);
 				}
 			}
 		}
